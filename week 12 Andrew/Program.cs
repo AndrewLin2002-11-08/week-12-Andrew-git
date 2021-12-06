4c48fe3c589e8524a8309c720f90cb2d5ce22582
AndrewLin2002-11-08 <andrewlin01@student.ciputra.ac.id>
2021-12-06 08:45:14 +0700

    Add project files.

---
+++ week 12 Andrew/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace week_12_Andrew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berapa Data : 1");
+            Console.WriteLine();
+            Console.WriteLine("data ke-1");
+            Console.WriteLine("NIM                      : 001");
+            Console.WriteLine("NAMA                     : Andrew Lin");
+            Console.WriteLine("JENIS KELAMIN            : Laki laki");
+            Console.WriteLine("TAHUN MASUK              : 2021");
+            Console.WriteLine("PROGRAM STUDI            : isb");
+            Console.WriteLine("KELAS                    : alpro");
+
+            Console.WriteLine("");
+            Console.WriteLine("PRINT HASIL ? (y/n) . . .");
+            
+           
+        }
+    }
+}
